467575dc80f46f573ebeba53072c249f3befc084
Tomohiro Uno <uuuteenet@gmail.com>
2023-05-10 12:56:31 +0900

    Read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("failed to read from standard input");
+    println!("{}", buffer);
 }
